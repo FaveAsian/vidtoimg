3e88c942b39b6e2201717ed70c4a9d8438c15182
FaveAsian <38636488+FaveAsian@users.noreply.github.com>
2019-05-25 17:25:41 -0400

    finished gui page

--- src/main/java/me/favasian/vidtoimg/gui.java
+++ src/main/java/me/favasian/vidtoimg/gui.java
@@ -1,13 +1,25 @@
 package me.favasian.vidtoimg;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class gui {
     private JFrame window;
     public gui(){
         //create instance of Jframe
         window = new JFrame("Converter");
+
+        //creates instance of gridbagconstraint
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //create instance of border
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+
+        //create imageicon for button
+        ImageIcon dots = new ImageIcon(getClass().getClassLoader().getResource("textures/dots.jpg"));
 
         //set window size and make window appear
         window.setSize(800,600);
@@ -24,7 +36,7 @@ public class gui {
         JPanel southPanel = new JPanel();
 
         //sets center layout to be grid layout
-        centerPanel.setLayout(new GridLayout(6,1));
+        centerPanel.setLayout(new GridBagLayout());
 
         /*Components*/
         //JLabels
@@ -46,17 +58,52 @@ public class gui {
         //JButtons
         JButton done = new JButton("Done");
 
-        //JTextField
-        JFormattedTextField frameJump = new JFormattedTextField();
-        frameJump.setColumns(20);
+        JButton mp4Btn = new JButton(resizeIcon(dots, 30, 20));
+        mp4Btn.setPreferredSize(new Dimension(30, 20));
 
+        JButton folderBtn = new JButton(resizeIcon(dots, 30, 20));
+        folderBtn.setPreferredSize(new Dimension(30, 20));
+
+        //JTextField
+        JTextField frameJump = new JTextField(5);
+        frameJump.setBorder(blackline);
+
+        //JTextArea
+        JTextArea mp4Dir = new JTextArea(1,15);
+        mp4Dir.setBorder(blackline);
+
+        JTextArea folderDir = new JTextArea(1,15);
+        folderDir.setBorder(blackline);
 
         /*add labels to directional panel*/
-        //center
-        centerPanel.add(addmp4);
-        centerPanel.add(addFolder);
-        centerPanel.add(addJump);
-        centerPanel.add(frameJump);
+        //center && adds position for each component
+        //gbc changes for addmp4
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        centerPanel.add(addmp4, gbc);
+        //gbc changes for mp4Dir
+        gbc.gridy++;
+        centerPanel.add(mp4Dir, gbc);
+        //gbc change for mp4 dots
+        gbc.gridx = 1;
+        centerPanel.add(mp4Btn, gbc);
+        //gbc change for addFolder
+        gbc.gridx = 0;
+        gbc.gridy++;
+        centerPanel.add(addFolder, gbc);
+        //gbc change for FolderDir
+        gbc.gridy++;
+        centerPanel.add(folderDir, gbc);
+        //gbc change for folder dot
+        gbc.gridx = 1;
+        centerPanel.add(folderBtn, gbc);
+        //gbc changes for addJump
+        gbc.gridx = 0;
+        gbc.gridy++;
+        centerPanel.add(addJump, gbc);
+        //gbc changes for frameJump
+        gbc.gridy++;
+        centerPanel.add(frameJump, gbc);
 
         //north
         northPanel.add(title);
@@ -69,7 +116,25 @@ public class gui {
         mainPanel.add(northPanel, BorderLayout.NORTH);
         mainPanel.add(southPanel, BorderLayout.SOUTH);
 
+        //set textarea to uneditable
+        mp4Dir.setEditable(false);
+        folderDir.setEditable(false);
+
         window.setContentPane(mainPanel);
         window.setVisible(true);
+
+        //add button actions
+        folderBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    //Resizes imageicon
+    private Icon resizeIcon(ImageIcon icon, int resizedWidth, int resizedHeight) {
+        Image img = icon.getImage();
+        Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight,  java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(resizedImage);
     }
 }
